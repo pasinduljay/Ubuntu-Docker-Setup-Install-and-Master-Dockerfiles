0687fa785efd82d798c75ba97fc474aacc5bee46
pasinduljay <pasindulakshitha0822@gmail.com>
2024-07-18 01:20:37 +0530

    bash script updated

--- install-docker-and-compose.sh
+++ install-docker-and-compose.sh
@@ -1,28 +1,17 @@
 #!/bin/bash
 
-# Update package list
 sudo apt update
 
-# Install Docker
 sudo curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh get-docker.sh >/dev/null 2>&1
 
-# Add the user to the docker group
-sudo usermod -aG docker $USER
-
-# Refresh group membership without stopping the script
-newgrp docker <<EOF
-# Commands to run after the group change
-echo "User added to docker group."
-
-# Download and install Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose 
 sudo chmod +x /usr/local/bin/docker-compose
+
+docker --version
+docker-compose --version
 
 echo "Docker and Docker Compose successfully installed!"
 
-# Display versions
-docker --version
-docker-compose --version
-EOF
+echo "Additinally add user to the docker group" 
 
-# Continue with any other commands outside the newgrp context if needed
+sudo usermod -aG docker $USER && newgrp docker
