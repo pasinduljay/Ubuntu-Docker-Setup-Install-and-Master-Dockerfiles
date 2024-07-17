e859bd5dded7c2a44b313bb522753c5310a70ad1
pasinduljay <pasindulakshitha0822@gmail.com>
2024-07-18 00:46:01 +0530

    bash script updated

--- install-docker-and-compose.sh
+++ install-docker-and-compose.sh
@@ -9,10 +9,7 @@ sudo apt update
 sudo apt-get install -y docker.io
 
 # Add current user to the docker group
-sudo usermod -aG docker $USER
-
-# Install Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose 
+sudo usermod -aG docker $USER && sudo newgrp docker && sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose 
 sudo chmod +x /usr/local/bin/docker-compose
 
 # Verify installation
